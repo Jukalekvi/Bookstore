cacc1971fea64f5906d0ef13b49a3e242677245b
Jukalekvi <bhi072@myy.haaga-helia.fi>
2024-10-02 12:57:10 +0300

    Third commit

--- src/main/java/hh/backend/jukka/com/bookstore/web/BookController.java
+++ src/main/java/hh/backend/jukka/com/bookstore/web/BookController.java
@@ -1,17 +1,28 @@
 package hh.backend.jukka.com.bookstore.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.backend.jukka.com.bookstore.domain.Book;
+import hh.backend.jukka.com.bookstore.repository.BookRepository;
 
 
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookRepository bookRepository;
 
-    @GetMapping("/index")
-    public String showIndex() {
-        return showIndex();
+    @GetMapping("/booklist")
+    public String showBookList(Model model) {
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
     }
     
 }
