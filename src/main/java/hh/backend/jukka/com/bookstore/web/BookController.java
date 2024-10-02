3f038d7503d30b016b5963fa94f4240322ea35ba
Jukalekvi <bhi072@myy.haaga-helia.fi>
2024-10-02 13:25:14 +0300

    Fourth commit

--- src/main/java/hh/backend/jukka/com/bookstore/web/BookController.java
+++ src/main/java/hh/backend/jukka/com/bookstore/web/BookController.java
@@ -6,9 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 import hh.backend.jukka.com.bookstore.domain.Book;
 import hh.backend.jukka.com.bookstore.repository.BookRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 
@@ -17,12 +24,35 @@ public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
-
+    
+    //Näyttää kirjat listassa
     @GetMapping("/booklist")
     public String showBookList(Model model) {
         List<Book> books = (List<Book>) bookRepository.findAll();
         model.addAttribute("books", books);
         return "booklist";
+    }
+    
+    //Näyttää lomakkeen uuden kirjan lisäämiseen
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    
+    //Käsittelee lomakkeen ja tallentaa uuden kirjan
+    @PostMapping("savebook")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+    
+
+    //Poistaa kirjan ID:n perusteella
+    @GetMapping("/deletebook/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:/booklist";
     }
     
 }
